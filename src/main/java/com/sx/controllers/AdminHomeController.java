4bd55d757e83cd01272b246907a14cd008ae0d3c
wsteenhuis81 <wsteenhuis@gmail.com>
2016-04-20 18:34:21 +0200

    spring boot mvc eerste opzet views in html/thymeleaf java files commented

--- src/main/java/com/sx/controllers/AdminHomeController.java
+++ src/main/java/com/sx/controllers/AdminHomeController.java
@@ -19,10 +19,44 @@ import java.util.List;
 @Controller
 public class AdminHomeController {
 
-    //Enum Admin_Options contain different admin tasks to chose from
-    //Admin_Options contains a discriptopn (to select in our vies) and a specific html target to redirect to
-    //values() returns an array of all enum values
+    //Enum Admin_Options contains different admin tasks to chose from
+    //each constant contains a discriptopn (to select in our views) and a specific html target to redirect to
+    private enum Admin_Options {
+
+        //TASKS:
+        TRAINER("Trainer toevoegen of wijzigen", "/edit_trainer"),
+        CUSTOMER("Klant toevoegen of wijzigen", "/edit_customer"),
+        SUBSCRIPTION("Abonnement aanmaken of wijzigen", "/edit_subscription");
+
+        private String option;
+        private String htmltarget;
+
+        //contructor (implicitly private):
+        Admin_Options(String option, String htmltarget){
+            this.option = option;
+            this.htmltarget = htmltarget;
+        }
+
+        //getters & setters
+        public String getOption() {
+            return option;
+        }
+
+        public void setOption(String option) {
+            this.option = option;
+        }
+
+        public String getHtmltarget() {
+            return htmltarget;
+        }
+
+        public void setHtmltarget(String htmltarget) {
+            this.htmltarget = htmltarget;
+        }
+    }
+
     //controller uses enum values in a List
+    //.values() returns an array of all enum values
     private List<Admin_Options>options = Arrays.asList(Admin_Options.values());
 
     //getter & setter:
@@ -35,14 +69,22 @@ public class AdminHomeController {
     }
 
 
+    //log in form uses th:action="@{/adminhome}" in thymeleaf
+    //when this url is requested admin_home.html will be shown
+    //admin_home is loaded with java object(s) through model.addAttributes("String identiefier", object)
+    //in this case the admin tasks(enum) are loaded in our view
     @RequestMapping("/adminhome")
     public String adminhome(Model model){
         model.addAttribute("admin_opts", options);
         return "/admin_home";
     }
 
+    //the form on admin_home uses th:action="@{/adminopts}" in thymeleaf
+    //this form sends user input (chosen Admin_Task) to this adress that can be read with method=RM.GET
     @RequestMapping(value="adminopts", method= RequestMethod.GET)
-    public String choseAdminAction(@ModelAttribute("optionsListId") Admin_Options chosen, Model model) {
+    //user input is identified in thymeleaf with name="optionsListId" and is bound to chosen-parameter
+    //because the Admin_Options has a html target String, the chosen parameter ensures redirection to the right view
+    public String choseAdminAction(@ModelAttribute("optionsListId") Admin_Options chosen) {
         System.out.println(chosen);
         return chosen.getHtmltarget();
     }
