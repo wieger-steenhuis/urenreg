bf522fa0a3e69e889394d322a1b5bad12d3a43b2
wsteenhuis81 <wsteenhuis@gmail.com>
2016-04-20 20:49:03 +0200

    spring boot mvc eerste opzet views in html/thymeleaf java files commented

--- src/main/java/com/sx/controllers/AdminHomeController.java
+++ src/main/java/com/sx/controllers/AdminHomeController.java
@@ -83,7 +83,7 @@ public class AdminHomeController {
     //this form sends user input (chosen Admin_Task) to this adress that can be read with method=RM.GET
     @RequestMapping(value="adminopts", method= RequestMethod.GET)
     //user input is identified in thymeleaf with name="optionsListId" and is bound to chosen-parameter
-    //because the Admin_Options has a html target String, the chosen parameter ensures redirection to the right view
+    //because the Admin_Options has a html target String, the chosen parameter takes care of redirecting to the right view
     public String choseAdminAction(@ModelAttribute("optionsListId") Admin_Options chosen) {
         System.out.println(chosen);
         return chosen.getHtmltarget();
