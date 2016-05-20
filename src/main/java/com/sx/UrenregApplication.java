807851091a6bee8ec6cfff7dc0ffc82182738918
udr013 <udr013@gmail.com>
2016-05-20 11:03:13 +0200

    mysql auto create

--- src/main/java/com/sx/UrenregApplication.java
+++ src/main/java/com/sx/UrenregApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrenregApplication {
+public class  UrenregApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrenregApplication.class, args);
